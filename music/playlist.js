436b3ab9ccb46cb01f7ae6df2c01b7d065ffb201
farzan8304 <farzankalantari.pg@gmail.com>
2023-08-23 10:24:37 +0330

    sleep test created & some changes in music login

--- music/playlist.js
+++ music/playlist.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const musicDirectory = "c:\\Users\\mohammad\\Desktop\\s-w\\music\\musics";
+const musicDirectory = ".\\music\\musics";
 
 fs.readdir(musicDirectory, (err, files) => {
   if (err) {
